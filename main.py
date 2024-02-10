2b1f5644298778ebc21c97b591564d4a3792c9de
DDQuin <ddquin420@gmail.com>
2024-02-10 00:31:57 +0000

    Added ESP config set

--- main.py
+++ main.py
@@ -48,10 +48,22 @@ async def saveConfig():
         "schedule": app.params.schedule
     })
 
+async def setESPConfig():
+    # response = requests.post(url=url, data=f"
+    #                          {app.params.saturation},{app.params.contrast},{app.params.brightness}
+    #                          ")
+    response = requests.post(url=f'{url}/config', json={
+        "brightness": app.params.brightness,
+        "saturation": app.params.saturation,
+        "contrast": app.params.contrast
+        })
+    if response.status_code != 200:
+        raise HTTPException("Something went wrong")
+
 
 async def captureImage():
     print("Capturing image from ESP")
-    response = requests.post(url=url, data=f"{app.params.saturation},{app.params.contrast},{app.params.brightness}")
+    response = requests.post(url=f'{url}/capture', data=f"{app.params.saturation},{app.params.contrast},{app.params.brightness}")
     if response.status_code != 200:
         raise HTTPException("Something went wrong")
     bytes_image_l = response.content
@@ -82,7 +94,7 @@ async def fuseAndUploadImages(bytes_image_l: bytes, bytes_image_r: bytes):
 
 ### APP SETUP ###
     
-url = os.getenv("MCC_URL", "http://localhost:8090/jpg")
+url = os.getenv("MCC_URL", "http://localhost:8090")
 app = FastAPI()
 origins = ["*"]
 
@@ -111,6 +123,7 @@ async def set_params(new_params: CameraParams) -> CameraParams:
     app.params = new_params
     setSchedule(app.params.schedule, captureImage)
     await saveConfig()
+    await setESPConfig()
     return app.params
 
 @app.get("/take_photo", description="Make MCC capture image")
